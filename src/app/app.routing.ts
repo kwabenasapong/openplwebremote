da7d78d2f946d8df7b183f4b4cafa0a093b9b276
Raoul Snyman <raoul@snyman.info>
2020-04-07 21:22:31 -0700

    Remove the hash from the URLs. I'm not sure why it was added.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     { path: 'themes', component: ThemesComponent}
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 
